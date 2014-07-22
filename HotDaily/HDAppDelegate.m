2820cba60e09263411550093213b86cf1e68b2f2
Weizhou <shiwoow@gmail.com>
2014-07-22 15:36:48 +0800

    colour adjustment

--- HotDaily/HDAppDelegate.m
+++ HotDaily/HDAppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     if (HDisPad) {
 //        UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
 //        UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
