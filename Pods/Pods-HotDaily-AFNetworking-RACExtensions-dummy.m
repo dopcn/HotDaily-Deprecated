115921863f064aca5323b8597b9d8ae2e163665f
weizhou <fengwz@dxyer.com>
2014-11-01 18:01:41 +0800

    update pods

--- Pods/Pods-HotDaily-AFNetworking-RACExtensions-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HotDaily_AFNetworking_RACExtensions : NSObject
-@end
-@implementation PodsDummy_Pods_HotDaily_AFNetworking_RACExtensions
-@end
--- Pods/Pods-HotDaily-SVPullToRefresh-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HotDaily_SVPullToRefresh : NSObject
-@end
-@implementation PodsDummy_Pods_HotDaily_SVPullToRefresh
-@end
