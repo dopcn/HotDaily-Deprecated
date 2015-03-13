97ba10ca4a06d91c4459246aff4b4b45422ba44e
weizhou <fengwz@dxyer.com>
2015-03-14 00:41:14 +0800

    side menu setup

---
+++ HotDaily/iPad/HDSideMenuTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  HDSideMenuTableViewController.h
+//  HotDaily
+//
+//  Created by weizhou on 3/1/15.
+//  Copyright (c) 2015 fengweizhou. All rights reserved.
+//
+
+#import "HDBaseViewController.h"
+
+@interface HDSideMenuTableViewController : HDBaseTableViewController
+
+@end
+
